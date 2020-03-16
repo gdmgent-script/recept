d1e6ba50e290d710e60607f4068241c0e5d0f04e
Frederick Roegiers <frederick@rogerthat.be>
2020-03-16 11:42:49 +0100

    [WIP]

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+// 1 create object --> const recipe = {...}
+
+// 2 add properties (naam, porties, ingrediënten)
+
+// 3 log every property
+// loop through ingredients to loop every individual ingredient
